2de4284c48c181dbbf051ae6932a92f31df2a060
te4a210889 <te4a210889@gmail.com>
2024-05-22 12:54:25 +0900

    aaa

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 //import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.core.model.Model;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 /**
 * Hello world!
 */
@@ -12,10 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HelloController
 {
-    @RequestMapping("/taro")
-    public String index() {
-        return "taro desu!";
-    }
+    //@RequestMapping("/taro")
+    //public String index() {
+        //return "taro desu!";
+    //}
     @RequestMapping("/miyagi")
     public String miyagi() {
         return "Miyagi desu!";
@@ -25,6 +27,19 @@ public class HelloController
     public String index(Model model) {
         //return "this is Spring Boot sample.";
         return "index";
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", "this is a setting message");
+        return "index";
+    }
+
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
     }
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -10,6 +10,11 @@
   <body>
     <h1>Index Page</h1>
     <p>this is sample page for Spring Boot!</p>
+    <p>${msg}</p>
+    <form method=“post” action=“/post”>
+      <input type=“text” name=“text1”>
+      <input type=“submit”>
+      </form>     
   </body>
 </html>
 
